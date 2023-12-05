fc446efbf6a45c742bba94a6bfc7dd52b0afbe3c
FaturRumahan <faturrumahan@gmail.com>
2023-12-05 15:13:10 +0700

    aster

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,15 @@ import UpdateProject from "./pages/dashboard/UpdateProject";
 
 const router = createBrowserRouter([
   { path: "/", element: <LandingPage /> },
-  {
-    path: "/admin-dashboard",
-    children: [
-      { path: "/admin-dashboard", element: <Dashboard /> },
-      { path: "/admin-dashboard/add", element: <AddProject /> },
-      { path: "/admin-dashboard/:idProject", element: <UpdateProject /> },
-    ],
-  },
+  { path: "/admin-dashboard", element: <Dashboard /> },
+  // {
+  //   path: "/admin-dashboard",
+  //   children: [
+  //     { path: "/admin-dashboard/", element: <Dashboard /> },
+  //     { path: "/admin-dashboard/add", element: <AddProject /> },
+  //     { path: "/admin-dashboard/:idProject", element: <UpdateProject /> },
+  //   ],
+  // },
 ]);
 
 function App() {
